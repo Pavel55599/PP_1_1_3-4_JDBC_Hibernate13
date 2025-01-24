f3abb67b4841d3c15182edd4423a32c4f0cf3678
Pavel Krykov <krykov.pavel@yandex.ru>
2025-01-24 14:09:15 +0300

    Добавлен класс UserDaoHibernateImpl

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,39 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.dao.UserDao;
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
+import jm.task.core.jdbc.util.Util;
+
+import java.sql.SQLException;
+
 public class Main {
-    public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+    public static void main(String[] args) throws SQLException {
+        Util util = new Util();
+        util.getConnection();
+
+
+        UserDao userDao = new UserDaoJDBCImpl();
+
+        userDao.createUsersTable();                                                 //создание таблицы юзеров
+
+        userDao.saveUser("ИВАН", "ВЕТРОВ", (byte) 20);               //добавление юзеров
+        userDao.saveUser("ФЕДОР", "ЕМЕЛЬЯНЕНКО", (byte) 25);
+        userDao.saveUser("ПАВЕЛ", "СИДОРОВ", (byte) 31);
+        userDao.saveUser("АЛЕКСЕЙ", "НИКОЛАЕВ", (byte) 38);
+
+
+        userDao.getAllUsers();                                                       //получение юзеров из БД
+
+        userDao.cleanUsersTable();                                                   //очистка таблицы юзеров
+
+        userDao.dropUsersTable();                                                    // удаление таблицы
+
     }
 }
+
+//1. Пакеты .mvn/wrapper .idea и target не должны попадать в коммит, добавь в gitignore и удали из коммита  ---- СДЕЛАЛ
+//2. class UserServiceImpl
+//private final UserDaoJDBCImpl userDao = new UserDaoJDBCImpl();
+//Наследуемся/инжектим, только самые общие классы/интерфейсы                                                ---- ВРОДЕ СДЕЛАЛ
+//3.  import java.sql.*; отключи вайлдкард в импортах                                                       ---- СДЕЛАЛ
+//4. Необходимо сделать рабочие транзакции самостоятельно, отключить autocommit.                            ---- СДЕЛАЛ
--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -1,35 +1,190 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
 
+//import java.sql.*;                //отключил вилдкарт
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
-public class UserDaoJDBCImpl implements UserDao {
+import java.sql.Connection;        // Добавил Явный импорт
+import java.sql.PreparedStatement; // Добавил Явный импорт
+import java.sql.SQLException;      // Добавил Явный импорт
+import java.sql.Statement;         // Добавил Явный импорт
+import java.sql.ResultSet;         // Добавил Явный импорт
+
+
+
+public class UserDaoJDBCImpl extends Util implements UserDao {
+
+
+
     public UserDaoJDBCImpl() {
 
     }
 
-    public void createUsersTable() {
+
+    public void createUsersTable() throws SQLException {                                 //создание таблицы юзеров
+        Connection connection = getConnection();
+
+        Statement stmt = null;
+
+        String sql = "CREATE TABLE IF NOT EXISTS users (ID INT AUTO_INCREMENT PRIMARY KEY, NAME VARCHAR(255), LASTNAME VARCHAR(255), AGE INT)";
+
+
+        try {
+            connection.setAutoCommit(false);                      // Отключаем autocommit
+            stmt = connection.createStatement();                  //ОСТАЛЬНОЙ КОД НЕ ТРОГАЕМ, ПРОСТО ДОБАВЛЯЕМ НЕСКОЛЬКО СТРОК
+            stmt.executeUpdate(sql);                              //конкретно три строки :  connection.setAutoCommit(false); connection.commit();   connection.rollback();
+            connection.commit();                                  //явно комитим
+        } catch (SQLException e) {
+            connection.rollback();                                //откатываем транзакцию в случае ошибки
+            throw new RuntimeException(e);                        //ОСТАЛЬНЫЕ МЕТОДЫ СДЕЛАЛ АНАЛОГИЧНО ЭТОМУ, КОММЕНТЫ НЕ ДОБАВЛЯЛ
+        } finally {
+            if (stmt != null) {
+                stmt.close();
+            }
+            if (connection != null) {
+                connection.close();
+            }
+        }
+    }
+    public void dropUsersTable() throws SQLException {                                 //удалить таблицу юзеров
+        Connection connection = getConnection();
+        Statement stmt = null;
+        String sql = "DROP TABLE IF EXISTS users";
+
+        try {
+            connection.setAutoCommit(false);
+            stmt = connection.createStatement();
+            stmt.executeUpdate(sql);
+            connection.commit();
+        } catch (SQLException e) {
+            connection.rollback();
+            throw new RuntimeException(e);
+        } finally {
+            if (stmt != null) {
+                stmt.close();
+            }
+            if (connection != null) {
+                connection.close();
+            }
+        }
+
 
     }
 
-    public void dropUsersTable() {
+    public void saveUser(String name, String lastName, byte age) throws SQLException { // сохранить юзера
+        if (name == null || name.isEmpty() || lastName == null || lastName.isEmpty()) {
+            throw new IllegalArgumentException("Имя и фамилия не могут быть пустыми");
+        }
+
+        Connection connection = getConnection();
+        PreparedStatement stmt = null;
+        String sql = "INSERT INTO users (NAME, LASTNAME, AGE) VALUES (?, ?, ?)";
+
+        try {
+            connection.setAutoCommit(false);
+            stmt = connection.prepareStatement(sql);
+            stmt.setString(1, name);
+            stmt.setString(2, lastName);
+            stmt.setByte(3, age);
+            stmt.executeUpdate();
+            connection.commit();
+            System.out.println("User с именем " + name + " добавлен в базу данных");
+        } catch (SQLException e) {
+            connection.rollback();
+            throw new RuntimeException(e);
+        } finally {
+            if (stmt != null) {
+                stmt.close();
+            }
+            if (connection != null) {
+                connection.close();
+            }
+        }
+    }
+
+
+    public void removeUserById(long id) throws SQLException {                           //удалить юзера по ID
+        Connection connection = getConnection();
+        PreparedStatement stmt = null;
+        String sql = "DELETE FROM users WHERE id = ?";
+
+        try {
+            connection.setAutoCommit(false);
+            stmt = connection.prepareStatement(sql);
+            stmt.setLong(1, id);
+            stmt.executeUpdate();
+            connection.commit();
+        } catch (SQLException e) {
+            connection.rollback();
+            throw new RuntimeException(e);
+        } finally {
+            if (stmt != null) {
+                stmt.close();
+            }
+            if (connection != null) {
+                connection.close();
+            }
+        }
 
     }
 
-    public void saveUser(String name, String lastName, byte age) {
+    public List<User> getAllUsers() throws SQLException {                                 //получить всех пользователей ,список юзеров
+        List<User> users = new ArrayList<>();
+        Connection connection = getConnection();
+        Statement stmt = null;
+        String sql = "SELECT * FROM users";
 
+        try {
+            connection.setAutoCommit(false);
+            stmt = connection.createStatement();
+            ResultSet rs = stmt.executeQuery(sql);
+            while (rs.next()) {
+                User user = new User();
+                user.setId(rs.getLong("id"));
+                user.setName(rs.getString("name"));
+                user.setLastName(rs.getString("lastname"));
+                user.setAge(rs.getByte("age"));
+                users.add(user);
+                System.out.println(user);
+            }
+            connection.commit();
+        } catch (SQLException e) {
+            connection.rollback();
+            throw new RuntimeException(e);
+        } finally {
+            if (stmt != null) {
+                stmt.close();
+            }
+            if (connection != null) {
+                connection.close();
+            }
+        }
+
+        return users;
     }
 
-    public void removeUserById(long id) {
+    public void cleanUsersTable() {                                                       //очистить таблицу пользователей
+        Connection connection = getConnection();
+        Statement stmt = null;
+        String sql = "DELETE FROM users";
 
-    }
-
-    public List<User> getAllUsers() {
-        return null;
-    }
-
-    public void cleanUsersTable() {
+        try {
+            connection.setAutoCommit(false);
+            stmt = connection.prepareStatement(sql);
+            stmt.executeUpdate(sql);
+            connection.commit();
+        } catch (SQLException e) {
+            try {
+                connection.rollback();
+            } catch (SQLException ex) {
+                throw new RuntimeException(ex);
+            }
+            throw new RuntimeException(e);
+        }
 
     }
 }
