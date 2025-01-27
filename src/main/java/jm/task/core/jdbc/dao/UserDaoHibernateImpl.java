3986db8c8781e88244fd202738e83d8cdaabdfe0
Pavel Krykov <krykov.pavel@yandex.ru>
2025-01-27 17:54:46 +0300

    Добавлен класс UserDaoHibernateImpl

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -1,42 +1,133 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
+import java.util.ArrayList;
 import java.util.List;
+//import static jm.task.core.jdbc.util.Util.sessionFactory;
 
-public class UserDaoHibernateImpl implements UserDao {
+//import static jm.task.core.jdbc.util.Util.sessionFactory;
+
+//import static jm.task.core.jdbc.util.Util.sessionFactory;
+
+public class UserDaoHibernateImpl extends Util  implements UserDao {
+    private SessionFactory sessionFactory;
     public UserDaoHibernateImpl() {
 
     }
 
-
     @Override
     public void createUsersTable() {
+        Transaction transaction = null;
 
+
+        String sql = "CREATE TABLE IF NOT EXISTS users (ID INT AUTO_INCREMENT PRIMARY KEY," +
+                " NAME VARCHAR(255), LASTNAME VARCHAR(255), AGE INT)";
+
+        try (Session session = Util.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+            session.createSQLQuery(sql).executeUpdate();
+            transaction.commit();
+        }catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+        }
     }
 
     @Override
     public void dropUsersTable() {
+        Transaction transaction = null;
+        String sql = "DROP TABLE IF EXISTS users";
+        try (Session session = Util.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+            session.createSQLQuery(sql).executeUpdate();
+            transaction.commit();
+        }catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
 
+            }
+        }
     }
+
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
+        Transaction transaction = null;
+        try (Session session = Util.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+            User user = new User();
+            user.setName(name);
+            user.setLastName(lastName);
+            user.setAge(age);
+            session.save(user);
+            transaction.commit();
+            System.out.println("User с именем " + name + " добавлен в базу данных через хибернейт"); //?????????
+        }catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+
+        }
 
     }
 
     @Override
     public void removeUserById(long id) {
+        Transaction transaction = null;
+        try (Session session = Util.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+            User user = session.get(User.class, id);
+            if (user != null) {                                           //вот из за этой проверки намучался ппц
+                session.delete(user);
+            }else {
+                System.out.println("пользователь не найден");
+            }
+
+            //session.delete(user);
+            transaction.commit();
+        }catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }
 
     }
 
     @Override
-    public List<User> getAllUsers() {
+    public List<User> getAllUsers() {                                        //этому методу вообще не пойму че надо
+        Transaction transaction = null;
+        try (Session session = Util.getSessionFactory().openSession()) {      //он все тесты валит
+            transaction = session.beginTransaction();                        //его тестить надо самым последним
+            List<User> users = session.createQuery("from User").list();
+            transaction.commit();
+            return users;
+        }catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+        }
         return null;
     }
 
     @Override
     public void cleanUsersTable() {
-
+        Transaction transaction = null;
+        try (Session session = Util.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+            session.createSQLQuery("DELETE FROM users").executeUpdate(); //и здесь намучался, ошибка была в том что в классе юзер не
+            transaction.commit();                                           //дал название в этой штуке @Table(name = "users")
+        }catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+        }
     }
 }
+
